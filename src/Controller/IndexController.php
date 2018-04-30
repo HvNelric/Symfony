2deaf787e759529a3f934b3bfaa872208e392418
HvN <37754127+HvNelric@users.noreply.github.com>
2018-04-30 17:10:30 +0200

    first up

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function index()
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+    
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function hello() {
+        return $this->render('index/hello.html.twig');
+    }
+    
+    /**
+     * 
+     * @Route("/bonjour/{qui}")
+     */
+    public function bonjour($qui){
+        return $this->render('index/bonjour.html.twig', [
+            'qui' => $qui
+        ]);
+    }
+    
+    /**
+     * 
+     * @Route("/salut/{qui}", defaults={"qui": "toi"})
+     * @param string $qui
+     */
+    public function salut($qui){
+        return $this->render('index/salut.html.twig', [
+            'qui' => $qui
+        ]);
+    }
+    
+    /**
+     * 
+     * @Route("/coucou/{firstname}-{lastname}", defaults={"lastname": ""})
+     * @param string $qui
+     */
+    public function coucou($firstname, $lastname){
+        $name = $firstname;
+        if($lastname == ''){
+            $name .= ' ' . $lastname;
+        }
+        
+        return $this->render('index/coucou.html.twig', [
+            'name' => $name
+        ]);
+    }
+    
+    /**
+     * @Route("/twig")
+     */
+    public function twig(){
+        return $this->render(
+                'index/twig.html.twig',
+                [
+                    'auj' => new \DateTime()
+                ]
+        );
+    }
+}
